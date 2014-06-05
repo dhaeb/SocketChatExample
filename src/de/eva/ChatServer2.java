1f806afbd0962a2c2f19af0042aee49e64464e1c
Dan Häberlein <dan.haeberlein@googlemail.com>
2014-06-05 10:11:32 +0200

    server2 with try finally for java < 7

---
+++ src/de/eva/ChatServer2.java
@@ -0,0 +1,40 @@
+package de.eva;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer2 {
+
+	private int port;
+	private ServerSocket server;
+	private boolean isShutdown;
+
+	public ChatServer2(int port) throws IOException {
+		this.port = port;
+		server = new ServerSocket();
+	}
+
+	public void startServer() throws IOException{
+		while (!isShutdown) {
+			server.bind(new InetSocketAddress("localhost", port));
+			Socket serverSideSocket = server.accept();
+			try {
+				InputStream is = serverSideSocket.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				System.out.println(br.readLine());
+			} finally {
+				serverSideSocket.close();
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new ChatServer2(666).startServer();
+	}
+	
+}
